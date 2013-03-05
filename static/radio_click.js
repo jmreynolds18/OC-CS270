7d8027e12b9d70392f7da5975617a23b309bccaf
Joshua Reynolds <reyno28@email.lcc.edu>
2013-03-05 17:41:00 -0500

    added assignment 5

---
+++ static/radio_click.js
@@ -0,0 +1,22 @@
+function planeChoice (plane){
+	switch(plane){
+	case 152:
+		alert("Your favorite color is red");
+		break;
+	case 172:
+		alert("Your favorite color Blue");
+		break;
+	case 182:
+		alert("Your favorite color is Yellow");
+		break;
+	case 210:
+		alert("Your favorite color is Orange");
+		break;
+	case 220:
+		alert("Your favorite color is Green");
+		break;
+	default:
+		alert("Error in Javascript function planeChoice");
+		break;
+	}
+}
